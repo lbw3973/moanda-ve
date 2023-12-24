4d906652b2408e04f88a8eafbc2a95301896bdce
LeeByoungWook <lbw3973@naver.com>
2023-12-25 00:54:18 +0900

    design: 메인페이지 제작

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+const Home = () => {
+  return (
+    <div>
+      <S.tempDIV>Home</S.tempDIV>
+    </div>
+  );
+};
+
+export default Home;
