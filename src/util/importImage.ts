fa4ba29271fce627f08b31d1e04087e68e324cd7
LeeByoungWook <lbw3973@naver.com>
2023-12-27 19:56:18 +0900

    feat: 이미지 경로 수정

---
+++ src/util/importImage.ts
@@ -0,0 +1,27 @@
+import image1 from "/Mobile/Main/img_1.jpg";
+import image2 from "/Mobile/Main/img_2.jpg";
+import image3 from "/Mobile/Main/img_3.jpg";
+import image4 from "/Mobile/Main/img_4.jpg";
+import image5 from "/Mobile/Main/img_5.jpg";
+import image6 from "/Mobile/Main/img_6.jpg";
+import image7 from "/Mobile/Main/img_7.jpg";
+import image8 from "/Mobile/Main/img_8.jpg";
+import image9 from "/Mobile/Main/img_9.jpg";
+import image10 from "/Mobile/Main/img_10.jpg";
+import image11 from "/Mobile/Main/img_11.jpg";
+import image12 from "/Mobile/Main/img_12.jpg";
+
+export const mainImageList = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+];
