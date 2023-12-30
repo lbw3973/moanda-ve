93e905657620c0348ca06bb9507d84aaff5def11
SedanLee <water8048@naver.com>
2023-12-31 01:57:20 +0900

    feat: 메뉴 데이터 추가

---
+++ src/MenuData.ts
@@ -0,0 +1,18 @@
+export const menuFinancierItems = [
+  { src: "../../../public/menu/finan_1.jfif", name: "바닐라" },
+  { src: "../../../public/menu/finan_2.jfif", name: "소금 초코" },
+  { src: "../../../public/menu/finan_4.jfif", name: "캐러멜 피칸" },
+  { src: "../../../public/menu/finan_3.jfif", name: "캐러멜 소금" },
+  { src: "../../../public/menu/finan_5.jfif", name: "고구마" },
+  { src: "../../../public/menu/finan_6.jfif", name: "호두과자" },
+];
+
+export const menuCookieItems = [
+  { src: "../../../public/menu/cookie_1.jfif", name: "플레인" },
+  { src: "../../../public/menu/cookie_2.jfif", name: "얼그레이 레몬" },
+  { src: "../../../public/menu/cookie_3.jfif", name: "사과 시나몬" },
+  { src: "../../../public/menu/cookie_4.jfif", name: "땅콩 베리" },
+  { src: "../../../public/menu/cookie_5.jfif", name: "래밍턴" },
+  { src: "../../../public/menu/cookie_6.jfif", name: "오레오 마시멜로우" },
+  { src: "../../../public/menu/cookie_7.jfif", name: "로투스 마시멜로우" },
+];
