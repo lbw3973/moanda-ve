1775bdc183ede490228d6c56c6176f235beda4e0
LeeByoungWook <lbw3973@naver.com>
2023-12-30 16:19:07 +0900

    feat: 헤더 반응형 디자인 구현 중

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,21 +1,29 @@
 import styled from "styled-components";
 
-export const Container = styled.div<{ $isScrolled: boolean }>`
+export const Container = styled.header<{ $isScrolled: boolean }>`
   width: 100%;
   height: ${props => (props.$isScrolled === true ? "60px" : "80px")};
 
   position: fixed;
   top: 0;
   z-index: 10;
-  background-color: ${props => (props.$isScrolled === true ? "#fff" : "transparent")};
-  ${props => props.$isScrolled && "box-shadow: 0px 4px 13px -5px rgba(50,50,93,0.25);"}
+  background-color: #fff9eb;
+  box-shadow: 0px 4px 13px -5px rgba(50, 50, 93, 0.25);
+
   display: flex;
   justify-content: center;
   align-items: center;
 
+  padding: 0 10px;
+
   font-family: "NanumSquareNeo";
 
-  transition-duration: 500ms;
+  transition-duration: 300ms;
+
+  /* 테블릿, 모바일 */
+  @media ${({ theme }) => theme.windowSize.medium} {
+    width: calc(100% - 20px);
+  }
 `;
 
 export const Wrapper = styled.div`
@@ -23,7 +31,6 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-
   font-weight: 600;
 
   .logo {
@@ -38,8 +45,40 @@ export const Wrapper = styled.div`
       cursor: pointer;
       padding: 4px;
       transition-duration: 300ms;
-      &:hover {
-        color: #654f43;
+      @media (hover: hover) {
+        &:hover {
+          color: #654f43;
+        }
+      }
+    }
+  }
+
+  /* 테블릿, 모바일 */
+  @media ${({ theme }) => theme.windowSize.medium} {
+    width: 100%;
+    .logo {
+      img {
+        width: 90px;
+      }
+    }
+
+    div {
+      span {
+        font-size: 14px;
+      }
+    }
+
+    @media ${({ theme }) => theme.windowSize.small} {
+      .logo {
+        img {
+          width: 75px;
+        }
+      }
+
+      div {
+        span {
+          font-size: 12px;
+        }
       }
     }
   }
