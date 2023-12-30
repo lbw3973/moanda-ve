5bcaf5e1e8f22febd56c1d194bac7bec3f2f336e
LeeByoungWook <lbw3973@naver.com>
2023-12-30 16:18:13 +0900

    feat: styled-components provider 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from "styled-components";
 import "./App.css";
 import Router from "./routes/Router";
+import theme from "./styles/theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,35 @@
+const windowSize = {
+  small: "screen and (max-width: 600px)",
+  medium: "screen and (max-width: 768px)",
+};
+
+const fontSize = {
+  xs: "0.5rem",
+  sm: "0.75rem",
+  base: "1rem",
+  md: "1.25rem",
+  lg: "1.5rem",
+};
+
+const lightversion = {
+  background: "#fff",
+  fontPrimary: "black",
+  fontSecondary: "gray",
+  primary: "#00a0ff",
+  secondary: "#ddd",
+  hover: "#00a0ff50",
+};
+
+const repo = {
+  open: "red",
+  close: "blue",
+};
+
+const theme = {
+  windowSize,
+  repo,
+  fontSize,
+  lightversion,
+};
+
+export default theme;
