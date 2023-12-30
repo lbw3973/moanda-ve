4e6080470745d15444acf18451fe85f66bd52fd5
LeeByoungWook <lbw3973@naver.com>
2023-12-30 16:18:32 +0900

    feat: 메인 이미지 스와이퍼 구현

--- src/components/Swiper/index.tsx
+++ src/components/Swiper/index.tsx
@@ -1,10 +1,29 @@
-import React from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import * as S from "./style";
+import { Navigation } from "swiper/modules";
+import { mainImageList } from "../../util/importImage";
 
 const Swiper = () => {
-  return <section></section>;
+  return (
+    <>
+      <S.swiper
+        modules={[Navigation]}
+        slidesPerView={1}
+        scrollbar={{ draggable: true }}
+        navigation
+        pagination={{ clickable: true }}
+        className="relative h-full"
+      >
+        {mainImageList.map((img, index) => (
+          <S.swiperSlide key={index}>
+            <img src={img} />
+          </S.swiperSlide>
+        ))}
+      </S.swiper>
+    </>
+  );
 };
 
 export default Swiper;
--- src/components/Swiper/style.ts
+++ src/components/Swiper/style.ts
@@ -1,3 +1,14 @@
 import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export const temp = styled.div``;
+
+export const swiper = styled(Swiper)`
+  width: 100%;
+`;
+
+export const swiperSlide = styled(SwiperSlide)`
+  img {
+    width: 100%;
+  }
+`;
