e4163b8f917eb7830af55f6afb3cd509f1f522ea
LeeByoungWook <lbw3973@naver.com>
2023-12-31 20:54:20 +0900

    feat: Home 페이지 style 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import Swiper from "../components/Swiper";
-import * as S from "./style";
 
 const Home = () => {
   return (
     <section>
       <Swiper />
-      <S.tempDIV>Home</S.tempDIV>
     </section>
   );
 };
--- src/pages/style.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const tempDIV = styled.div`
-  height: 2000px;
-`;
