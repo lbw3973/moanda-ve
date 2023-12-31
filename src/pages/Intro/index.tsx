fbd9a7a244eb74e8b17542c7b6f7fbf2549d472e
LeeByoungWook <lbw3973@naver.com>
2024-01-01 02:59:13 +0900

    feat: 카페 소개 페이지 생성 및 라우터 추가

---
+++ src/pages/Intro/index.tsx
@@ -0,0 +1,5 @@
+const Intro = () => {
+  return <div>index</div>;
+};
+
+export default Intro;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Layout from "../components/Layout";
+import Intro from "../pages/Intro";
 
 function Router() {
   return (
@@ -8,6 +9,7 @@ function Router() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/intro" element={<Intro />} />
         </Route>
       </Routes>
     </BrowserRouter>
