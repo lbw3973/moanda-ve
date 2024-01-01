12f032eee02ddb823be91da524ab941c0eab6508
LeeByoungWook <lbw3973@naver.com>
2024-01-01 14:29:57 +0900

    feat: NotFound 페이지 파일 생성

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Layout from "../components/Layout";
 import Intro from "../pages/Intro";
+import NotFound from "../pages/NotFound";
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/intro" element={<Intro />} />
+          <Route path="/*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
