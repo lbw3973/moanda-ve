29451e85d55bfe1be0d8d6a406210ce8806e677c
LeeByoungWook <lbw3973@naver.com>
2024-01-02 13:49:56 +0900

    remove: index.css 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import "./font/font.css";
