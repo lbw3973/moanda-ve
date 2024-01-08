c45963f6009a4fe434a33be34fcf70d317ee85e8
LeeByoungWook <lbw3973@naver.com>
2024-01-08 20:50:10 +0900

    feat:  페이지 이동시 스크롤 위치 초기화 구현

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,11 +2,13 @@ import Header from "../Header";
 import { Outlet } from "react-router-dom";
 import * as S from "./style";
 import Footer from "../Footer";
+import ScrollToTop from "../Common/ScrollToTop";
 
 const Layout = () => {
   return (
     <>
       <Header />
+      <ScrollToTop />
       <S.Main>
         <Outlet />
       </S.Main>
