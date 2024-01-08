f7e0e14da828490893da9c10f1245970a5349739
LeeByoungWook <lbw3973@naver.com>
2024-01-08 16:26:04 +0900

    fix: 공지사항 페이지를 답례품 페이지로 변경

--- src/pages/Notice/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as S from "./style";
-
-const Notice = () => {
-  return <S.NoticeSection>Notice</S.NoticeSection>;
-};
-
-export default Notice;
--- src/pages/Notice/style.ts
+++
@@ -1,3 +0,0 @@
-import styled from "styled-components";
-
-export const NoticeSection = styled.section``;
