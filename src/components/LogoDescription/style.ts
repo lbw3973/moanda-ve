045f9b154435858daeeeb1ddfe56e3adbba054fa
LeeByoungWook <lbw3973@naver.com>
2024-01-10 13:59:00 +0900

    feat: 카페소개의 로고 소개 제작

---
+++ src/components/LogoDescription/style.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const DescriptionContaier = styled.div`
+  text-align: center;
+  font-family: "NanumSquareNeo";
+  padding-bottom: 30px;
+  margin: 0 20px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+`;
+
+export const LogoImage = styled.img`
+  width: 300px;
+  padding-bottom: 30px;
+
+  @media ${({ theme }) => theme.windowSize.medium} {
+    width: 250px;
+  }
+
+  @media ${({ theme }) => theme.windowSize.small} {
+    width: 200px;
+  }
+`;
+
+export const InformContainer = styled.div`
+  font-size: 20px;
+  color: ${({ theme }) => theme.color.primary_normal};
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  .solving {
+    span {
+      background-color: #fdf89a;
+      border-radius: 100%;
+    }
+  }
+  .first_des,
+  .since {
+    @media ${({ theme }) => theme.windowSize.medium} {
+      font-size: 14px;
+    }
+
+    @media ${({ theme }) => theme.windowSize.small} {
+      font-size: 12px;
+    }
+  }
+  .since {
+  }
+`;
--- src/pages/Intro/index.tsx
+++ src/pages/Intro/index.tsx
@@ -1,8 +1,11 @@
 import WayToCome from "@/components/WayToCome";
 import * as S from "./style";
+import LogoDescription from "@/components/LogoDescription";
+
 const Intro = () => {
   return (
     <S.IntroSection>
+      <LogoDescription />
       <WayToCome />
     </S.IntroSection>
   );
