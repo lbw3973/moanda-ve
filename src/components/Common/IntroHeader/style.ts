faef26796c0ecc6df2100f74c93446d11bd1cd6c
LeeByoungWook <lbw3973@naver.com>
2024-01-10 13:58:21 +0900

    feat: Intro의 부제목을 컴포넌트로 분리

---
+++ src/components/Common/IntroHeader/style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 40px 0 20px;
+  font-family: "GmarketSans";
+  font-size: 30px;
+  color: ${({ theme }) => theme.color.primary_normal};
+
+  @media ${({ theme }) => theme.windowSize.medium} {
+    font-size: 24px;
+  }
+
+  h2::after {
+    content: "";
+    display: block;
+    width: 75%;
+    height: 1px;
+    margin-top: 10px;
+    background-color: rgba(0, 0, 0, 0.2);
+    transform: translateX(15%);
+  }
+`;
