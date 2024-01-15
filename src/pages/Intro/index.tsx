5637432a25e27054e897a84752b9b797d77c22e1
LeeByoungWook <lbw3973@naver.com>
2024-01-16 01:33:37 +0900

    feat: 카페소개 페이지에 스크롤에 따른 컴포넌트 등장 애니메이션 추가

--- src/pages/Intro/index.tsx
+++ src/pages/Intro/index.tsx
@@ -1,14 +1,45 @@
-import WayToCome from "@/components/WayToCome";
 import * as S from "./style";
+import WayToCome from "@/components/WayToCome";
 import LogoDescription from "@/components/LogoDescription";
 import VeganDescription from "@/components/VeganDescription";
+import { useEffect, useRef } from "react";
+
+const sections = [<LogoDescription />, <VeganDescription />, <WayToCome />];
 
 const Intro = () => {
+  const LogoDesRef = useRef<HTMLDivElement>(null);
+  const VeganDesRef = useRef<HTMLDivElement>(null);
+  const WayToComeRef = useRef<HTMLDivElement>(null);
+  const refs = [LogoDesRef, VeganDesRef, WayToComeRef];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      refs.forEach(ref => {
+        const offset = ref.current?.getBoundingClientRect().y as number;
+        if (window.scrollY + window.innerHeight >= offset) {
+          ref.current?.classList.add("visible");
+        }
+      });
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <S.IntroSection>
-      <LogoDescription />
+      {sections.map((section, index) => (
+        <div key={index} ref={refs[index]}>
+          {section}
+        </div>
+      ))}
+      {/* <LogoDescription />
       <VeganDescription />
-      <WayToCome />
+      <WayToCome /> */}
     </S.IntroSection>
   );
 };
--- src/pages/Intro/style.ts
+++ src/pages/Intro/style.ts
@@ -1,11 +1,48 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeInMove = keyframes`
+0% {
+  opacity: 0;
+  right: -30vw;
+}
+100% {
+  opacity: 1;
+  right: 0;
+}
+`;
+
+const fadeInMoveMobile = keyframes`
+0% {
+  opacity: 0;
+  right: -50vw;
+}
+100% {
+  opacity: 1;
+  right: 0;
+}
+`;
 
 export const IntroSection = styled.section`
   padding-top: 20px;
   margin: 0 auto;
+  overflow: hidden;
+  visibility: hidden;
 
   @media ${({ theme }) => theme.windowSize.large} {
     width: 1000px;
     padding: 20px 20px 0;
+  }
+
+  .visible {
+    visibility: visible;
+    opacity: 0;
+    position: relative;
+    right: -100px;
+    @media ${({ theme }) => theme.windowSize.large} {
+      animation: ${fadeInMove} 1s forwards;
+    }
+    @media ${({ theme }) => theme.windowSize.medium} {
+      animation: ${fadeInMoveMobile} 1s forwards;
+    }
   }
 `;
