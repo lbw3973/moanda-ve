684e2ed86c708b24482c1a0fd08bd8037a7c3e23
LeeByoungWook <lbw3973@naver.com>
2024-01-16 00:59:36 +0900

    feat: 비건소개 컴포넌트 구현

--- src/pages/Intro/index.tsx
+++ src/pages/Intro/index.tsx
@@ -1,11 +1,13 @@
 import WayToCome from "@/components/WayToCome";
 import * as S from "./style";
 import LogoDescription from "@/components/LogoDescription";
+import VeganDescription from "@/components/VeganDescription";
 
 const Intro = () => {
   return (
     <S.IntroSection>
       <LogoDescription />
+      <VeganDescription />
       <WayToCome />
     </S.IntroSection>
   );
