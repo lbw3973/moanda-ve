9668a1fba15d9100fd96a32e0d8faa806424bd4b
LeeByoungWook <lbw3973@naver.com>
2024-02-26 21:40:39 +0900

    feat: 해당 컴포넌트의 props로 사진을 전달받아 재활용하게끔 기능 추가

--- src/components/Swiper/index.tsx
+++ src/components/Swiper/index.tsx
@@ -2,30 +2,27 @@ import "swiper/css";
 import "swiper/css/pagination";
 import * as S from "./style";
 import { Autoplay, Pagination } from "swiper/modules";
-import { mainImageList_Mobile } from "../../constants/importImage";
 
-const Swiper = () => {
+const Swiper = ({ imageList }: { imageList: string[] }) => {
   return (
-    <>
-      <S.swiper
-        modules={[Pagination, Autoplay]}
-        slidesPerView={1}
-        scrollbar={{ draggable: true }}
-        navigation
-        pagination={{ clickable: true }}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
-        className="relative h-full"
-      >
-        {mainImageList_Mobile.map((img, index) => (
-          <S.swiperSlide key={index}>
-            <img src={img} />
-          </S.swiperSlide>
-        ))}
-      </S.swiper>
-    </>
+    <S.swiper
+      modules={[Pagination, Autoplay]}
+      slidesPerView={1}
+      scrollbar={{ draggable: true }}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: false,
+      }}
+      className="relative h-full"
+    >
+      {imageList.map((img, index) => (
+        <S.swiperSlide key={index}>
+          <img src={img} />
+        </S.swiperSlide>
+      ))}
+    </S.swiper>
   );
 };
 
--- src/components/Swiper/style.ts
+++ src/components/Swiper/style.ts
@@ -14,8 +14,30 @@ export const swiper = styled(Swiper)`
   .swiper-pagination-bullet-active {
     background-color: ${({ theme }) => theme.color.primary_normal};
   }
-  @media ${({theme}) => theme.windowSize.medium} {
-    height: 520px;
+  @media ${({ theme }) => theme.windowSize.large} {
+    height: calc(100vh - 80px);
+    overflow-y: hidden;
+
+    .swiper-slide-active {
+      position: relative;
+      top: 0;
+      height: calc(100vh - 80px);
+      width: 100%;
+
+      img {
+        display: block;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: auto;
+        max-width: inherit;
+        height: 100%;
+      }
+    }
+  }
+  @media ${({ theme }) => theme.windowSize.medium} {
+    height: 100%;
   }
 `;
 
