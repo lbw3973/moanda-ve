5cd62b2dbccc8c82784120534b6348b595f01f52
LeeByoungWook <lbw3973@naver.com>
2024-02-27 17:54:47 +0900

    design: 프로그래스 바 디자인 변경

--- src/components/Common/FullPageScroll/style.ts
+++ src/components/Common/FullPageScroll/style.ts
@@ -30,9 +30,14 @@ export const ProgressBarContainer = styled.div`
   transform: translateY(-50%);
   right: 100px;
   z-index: 10;
+
+  padding: 10px 15px;
+  background: rgba(255, 255, 255, 0.7);
+  border-radius: 6px;
 `;
 
 export const Wrapper = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   gap: 3rem;
@@ -40,7 +45,7 @@ export const Wrapper = styled.div`
 
 export const Elements = styled.div<{ $isCurrent: boolean }>`
   transition: background-color 500ms;
-  background-color: ${({ $isCurrent }) => ($isCurrent ? "rgba(219,12,31,0.8)" : "white")};
+  background-color: ${({ $isCurrent }) => ($isCurrent ? "rgba(234,12,31,1)" : "white")};
   padding: 6px;
   border-radius: 100%;
   cursor: pointer;
@@ -64,8 +69,10 @@ export const VerticalBar = styled.div`
   transform: translate(-50%, -50%);
   width: 5px;
   height: 99%;
-  background-color: rgba(255, 255, 255, 0.5);
+  background-color: rgba(255, 255, 255, 1);
   z-index: 10;
+
+  overflow: hidden;
 `;
 
 export const FillVerticalBar = styled.div<{ $curFilled: number; $totalEls: number }>`
