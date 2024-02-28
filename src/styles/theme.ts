04b514a399aba7d7c502a87af8d8313137841327
LeeByoungWook <lbw3973@naver.com>
2024-02-28 21:33:51 +0900

    feat: window size 추가 진행 중

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,15 @@
 const windowSize = {
+  xsmall: "screen and (max-width: 375px)",
   small: "screen and (max-width: 450px)",
   medium: "screen and (max-width: 768px)",
   large: "screen and (min-width: 769px)",
+  // large: "screen and (max-width: 1024px)",
+  // xlarge: "screen and (max-width: 1280px)",
+  // txlarge: "screen and (max-width: 1600px)",
+  // full: "screen and (min-width: 1601px)",
+  // xlarge: "screen and (min-width: 1024px)",
+  // txlarge: "screen and (min-width: 1280px)",
+  // full: "screen and (max-width: 1281px)",
 };
 
 const fontSize = {
