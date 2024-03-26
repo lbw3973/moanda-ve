f4900ff5d5f55d77340d2172d065f3ad5c27bafa
bwlee <lbw3973@naver.com>
2024-03-26 16:29:39 +0900

    test: 모바일 테스트

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,24 @@
 import { FullPageScroll } from "@/components/Common/FullPageScroll";
 import Footer from "@/components/Footer";
-import Swiper from "@/components/Swiper";
+// import Swiper from "@/components/Swiper";
 import { useScreenSizeStore } from "@/stores/screenSizeStore";
-import { mainImageList_Mobile } from "../../constants/importImage";
+// import { mainImageList_Mobile } from "../../constants/importImage";
 import Intro from "@/components/Main/DeskTop/Intro";
 import NewProduct from "@/components/Main/DeskTop/NewProduct";
 import PreviewGift from "@/components/Main/DeskTop/PreviewGift";
 import PreviewInsta from "@/components/Main/DeskTop/PreviewInsta";
-import { ISwiperProps } from "@/types/swiper";
-import { Autoplay, Pagination } from "swiper/modules";
+// import { ISwiperProps } from "@/types/swiper";
+// import { Autoplay, Pagination } from "swiper/modules";
 
-const swiperProps: ISwiperProps = {
-  imageList: mainImageList_Mobile,
-  modules: [Pagination, Autoplay],
-  height: "calc(100vh-80px)",
-  width: "100%",
-  pagination: true,
-  spaceBetween: 0,
-  useNavigation: false,
-};
+// const swiperProps: ISwiperProps = {
+//   imageList: mainImageList_Mobile,
+//   modules: [Pagination, Autoplay],
+//   height: "100%",
+//   width: "100%",
+//   pagination: true,
+//   spaceBetween: 0,
+//   useNavigation: false,
+// };
 
 const Home = () => {
   const { isMobileView } = useScreenSizeStore();
@@ -26,7 +26,12 @@ const Home = () => {
   return (
     <section style={{ position: "relative" }}>
       {isMobileView ? (
-        <Swiper props={swiperProps} />
+        <>
+          <Intro />
+          <NewProduct />
+          <PreviewGift />
+          <PreviewInsta />
+        </>
       ) : (
         <FullPageScroll>
           <Intro />
