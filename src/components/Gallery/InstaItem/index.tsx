49f0eea4a35034b220b29b74589d0854672f67b7
LeeByoungWook <lbw3973@naver.com>
2024-04-03 01:44:29 +0900

    feat: 인스타그램 개별 아이템 컴포넌트 구현

---
+++ src/components/Gallery/InstaItem/index.tsx
@@ -0,0 +1,42 @@
+import { IResInstagram } from "@/types/insta";
+import * as S from "./style";
+import { IoIosBrowsers } from "react-icons/io";
+import { BiMoviePlay } from "react-icons/bi";
+import { useState } from "react";
+import InstaModal from "../InstaModal";
+
+const InstaItem = ({ data }: { data: IResInstagram }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  return (
+    <>
+      <S.Container
+        onClick={() => setIsOpenModal(true)}
+        onMouseOver={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        $isHovered={isHovered}
+      >
+        {data.media_type === "VIDEO" ? (
+          data.thumbnail_url && <video src={data.media_url} autoPlay muted loop />
+        ) : (
+          <img src={data.media_url} />
+        )}
+        {data.media_type !== "IMAGE" && (
+          <S.SVGWrapper>
+            {data.media_type === "CAROUSEL_ALBUM" ? (
+              <IoIosBrowsers color="#fff" size={30} />
+            ) : (
+              <BiMoviePlay color="#fff" size={30} />
+            )}
+          </S.SVGWrapper>
+        )}
+        <S.PreviewCaption $isHovered={isHovered}>
+          <div className="caption">{data.caption}</div>
+        </S.PreviewCaption>
+      </S.Container>
+      {isOpenModal && <InstaModal data={data} closeModal={() => setIsOpenModal(false)} />}
+    </>
+  );
+};
+
+export default InstaItem;
