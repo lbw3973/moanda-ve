ba0680902884baf3a4dea790ac693a9e9e1cb864
LeeByoungWook <lbw3973@naver.com>
2024-04-03 01:43:33 +0900

    feat: axios 인스턴스 및 instagram 불러오기 api 구현

--- src/apis/instagram.ts
+++ src/apis/instagram.ts
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { instance } from "./axios";
+import { IResInstagramList } from "@/types/insta";
 
 export const getUserId = async () => {
   const res = await instance.get(`/me?fields=id,username&access_token=${import.meta.env.VITE_MOANDAVE_TOKEN}`);
@@ -7,9 +9,16 @@ export const getUserId = async () => {
 
 export const getInstagramPost = async () => {
   const res = await instance.get(
-    `${import.meta.env.VITE_MOANDAVE_USER_ID}/media?fields=id,media_type,media_url,caption&access_token=${
+    `${
+      import.meta.env.VITE_MOANDAVE_USER_ID
+    }/media?fields=id,media_type,media_url,thumbnail_url,caption,timestamp,children{media_type,media_url,permalink}&access_token=${
       import.meta.env.VITE_MOANDAVE_TOKEN
-    }`,
+    }&limit=12`,
   );
-  return res;
+  return res.data;
+};
+
+export const getInstagramNextPost = async (url: string): Promise<IResInstagramList> => {
+  const res = await axios.get(url);
+  return res.data;
 };
