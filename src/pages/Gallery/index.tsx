f54bca6559d0dcafbd5195a4499482390299b6a6
LeeByoungWook <lbw3973@naver.com>
2024-04-03 01:45:38 +0900

    feat: 갤러리 페이지 구현

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -1,7 +1,12 @@
+import InstaList from "@/components/Gallery/InstaList";
 import * as S from "./style";
 
 const Gallery = () => {
-  return <S.GallerySection>Gallery</S.GallerySection>;
+  return (
+    <S.GallerySection>
+      <InstaList />
+    </S.GallerySection>
+  );
 };
 
 export default Gallery;
