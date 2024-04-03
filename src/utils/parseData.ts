5baa3f775ab5b2575b12d490820c5c8851995af1
LeeByoungWook <lbw3973@naver.com>
2024-04-04 01:57:34 +0900

    feat: 인스타 게시물 날짜 형식 수정

--- src/utils/parseData.ts
+++ src/utils/parseData.ts
@@ -3,5 +3,5 @@ export const getUploadDate = (date: Date) => {
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
-  return `${year}년 ${month}월 ${day}일`;
+  return `${month}월 ${day}일, ${year}년 `;
 };
