c458b8018e216bd992b6a4af7a761854dc1dd8cc
SedanLee <water8048@naver.com>
2024-04-13 01:08:01 +0900

    feat: 통합에러 수정

--- src/components/Home/PreviewGift/style.ts
+++ src/components/Home/PreviewGift/style.ts
@@ -2,34 +2,40 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   position: relative;
-  height: 100%;
+  height: calc(100vh - 80px);
+  width: 100%;
+  text-align: center;
+  overflow: hidden;
 `;
 
-export const ContentWrapper = styled.div`
-  h2 {
-    position: absolute;
-    top: 5%;
-    font-family: "Catamaran";
-    letter-spacing: 2px;
-    font-size: 80px;
-    font-weight: 600;
-    line-height: 1.2;
-    z-index: 100;
-    text-align: left;
+export const GiftHeader = styled.h2`
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  font-family: "TTLaundryGothicB";
+  letter-spacing: 2px;
+  font-size: 80px;
+  font-weight: 600;
+  line-height: 1.2;
+  z-index: 10;
+  text-align: left;
 
-    span {
-      display: block;
-      padding-left: 120px;
+  @media ${({ theme }) => theme.windowSize.txlarge} {
+    left: 40%;
+  }
 
-      &::after {
-        content: "";
-        display: inline-block;
-        width: 20px;
-        height: 20px;
-        background-color: red;
-        margin-left: 16px;
-      }
-    }
+  @media ${({ theme }) => theme.windowSize.xlarge} {
+    left: 30%;
+  }
+
+  @media ${({ theme }) => theme.windowSize.large} {
+    left: 20%;
+  }
+
+  span {
+    display: block;
+    padding-left: 120px;
+    white-space: nowrap;
   }
 `;
 
@@ -42,32 +48,27 @@ export const SwiperContainer = styled.div`
 `;
 
 export const SwiperWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  max-height: 550px;
+  transform: translate(-50%, -50%);
+  height: 80%;
   width: 100%;
-  height: 1000px;
-  display: flex;
-  align-items: center;
+
+  max-width: 1360px;
   overflow: hidden;
-  position: relative;
-  .swiper {
-    position: absolute;
-    left: -500px;
-    width: 100%;
-    height: 500px;
-    .swiper-wrapper {
-      .swiper-slide {
-      }
-      .swiper-slide-active {
-        img {
-          width: 400px;
-        }
-      }
-    }
+
+  .swiper-button-prev,
+  .swiper-button-next {
+    display: none;
   }
 
   .swiper {
     .swiper-wrapper {
       align-items: end;
       margin-left: 92px;
+
       .swiper-slide {
         width: 215px !important;
         height: 50%;
@@ -75,6 +76,8 @@ export const SwiperWrapper = styled.div`
         justify-content: center;
         img {
           border-radius: 12px;
+          width: 100%;
+          height: 100%;
         }
       }
       .swiper-slide-active {
