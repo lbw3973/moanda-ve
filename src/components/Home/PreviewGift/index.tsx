18eb687d776116c3d90026f3f30d9fb1d6859e96
SedanLee <water8048@naver.com>
2024-04-13 00:52:09 +0900

    feat: 통합

--- src/components/Home/PreviewGift/index.tsx
+++ src/components/Home/PreviewGift/index.tsx
@@ -1,24 +1,7 @@
-import Swiper from "@/components/Common/Swiper";
 import * as S from "./style";
-import { ISwiperProps } from "@/types/swiper";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import IntroGiftItem from "@/constants/IntroGift.json";
-import { Autoplay } from "swiper/modules";
-import { FaHeart } from "react-icons/fa";
-
-const swiperProps: ISwiperProps = {
-  imageList: Object.entries(IntroGiftItem).map(([, value]) => {
-    return value;
-  }),
-  modules: [Autoplay],
-  height: "100%",
-  width: "100%",
-  pagination: false,
-  spaceBetween: 80,
-  useNavigation: false,
-  slideperView: 4,
-  loop: true,
-  useTouchSlide: true,
-};
 
 const test = Object.entries(IntroGiftItem).map(([, value]) => {
   return value;
@@ -28,18 +11,29 @@ console.log(test);
 const PreviewGift = () => {
   return (
     <S.Container>
-      <S.GiftHeader>
-        Moanda've
-        <br />
-        <span>
-          Gift Ideas <FaHeart color="red" size={30} />
-        </span>
-      </S.GiftHeader>
-      <S.SwiperContainer>
-        <S.SwiperWrapper>
-          <Swiper props={swiperProps} />
-        </S.SwiperWrapper>
-      </S.SwiperContainer>
+      <S.ContentWrapper>
+        <h2>
+          Moanda've
+          <br />
+          <span>Gift Ideas</span>
+        </h2>
+      </S.ContentWrapper>
+      <S.SwiperWrapper>
+        <Swiper
+          className="swiper"
+          modules={[Navigation]}
+          slidesPerView={4}
+          loop={true}
+          spaceBetween={10}
+          centeredSlides={true}
+        >
+          {Object.entries(IntroGiftItem).map(([title, data], index) => (
+            <SwiperSlide key={index}>
+              <img src={data} alt={title} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </S.SwiperWrapper>
     </S.Container>
   );
 };
--- src/components/Home/PreviewGift/style.ts
+++ src/components/Home/PreviewGift/style.ts
@@ -2,40 +2,34 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   position: relative;
-  height: calc(100vh - 80px);
-  width: 100%;
-  text-align: center;
-  overflow: hidden;
+  height: 100%;
 `;
 
-export const GiftHeader = styled.h2`
-  position: absolute;
-  top: 10%;
-  left: 50%;
-  font-family: "TTLaundryGothicB";
-  letter-spacing: 2px;
-  font-size: 80px;
-  font-weight: 600;
-  line-height: 1.2;
-  z-index: 10;
-  text-align: left;
+export const ContentWrapper = styled.div`
+  h2 {
+    position: absolute;
+    top: 5%;
+    font-family: "Catamaran";
+    letter-spacing: 2px;
+    font-size: 80px;
+    font-weight: 600;
+    line-height: 1.2;
+    z-index: 100;
+    text-align: left;
 
-  @media ${({ theme }) => theme.windowSize.txlarge} {
-    left: 40%;
-  }
+    span {
+      display: block;
+      padding-left: 120px;
 
-  @media ${({ theme }) => theme.windowSize.xlarge} {
-    left: 30%;
-  }
-
-  @media ${({ theme }) => theme.windowSize.large} {
-    left: 20%;
-  }
-
-  span {
-    display: block;
-    padding-left: 120px;
-    white-space: nowrap;
+      &::after {
+        content: "";
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        background-color: red;
+        margin-left: 16px;
+      }
+    }
   }
 `;
 
@@ -48,20 +42,26 @@ export const SwiperContainer = styled.div`
 `;
 
 export const SwiperWrapper = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-
-  transform: translate(-50%, -50%);
-  height: 80%;
   width: 100%;
-
-  max-width: 1360px;
+  height: 1000px;
+  display: flex;
+  align-items: center;
   overflow: hidden;
-
-  .swiper-button-prev,
-  .swiper-button-next {
-    display: none;
+  position: relative;
+  .swiper {
+    position: absolute;
+    left: -500px;
+    width: 100%;
+    height: 500px;
+    .swiper-wrapper {
+      .swiper-slide {
+      }
+      .swiper-slide-active {
+        img {
+          width: 400px;
+        }
+      }
+    }
   }
 
   .swiper {
