fe23e5adbf050fedd806dc7998c8a78847198192
LeeByoungWook <lbw3973@naver.com>
2024-04-23 15:02:49 +0900

    feat: vercel analytics 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import GlobalStyles from "./styles/GlobalStyle.ts";
 import "./font/font.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalStyles />
     <App />
+    <Analytics />
   </React.StrictMode>,
 );
